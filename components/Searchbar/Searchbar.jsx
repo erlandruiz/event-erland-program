44305c49d5a2f131cf469abc2e05de2b9c737590
Erland Ruiz <erlandruiz@hotmail.com>
2025-12-04 00:25:39 -0500

    twenty one

--- components/Searchbar/Searchbar.jsx
+++ components/Searchbar/Searchbar.jsx
@@ -7,7 +7,7 @@ import ThemeToggle from "../ThemeToggle";
 const Searchbar = () => {
   const { handleSubmit } = useContext(EventContext);
   return (
-    <div className="w-[90vw] sm:w-[60vw] md:w-[50vw] lg:w-[40vw] xl:w-max p-8 xl:pl-8 xl:pr-2 h:auto xl:h-[70px] rounded-3xl xl:rounded-full backdrop-blur-[20px] flex flex-col xl:flex-row items-center gap-6 mx-auto text-sm  bg-[var(--color-secondary)]/80 text-white border boder-[var(--color-tertiary-hover)]">
+    <div className="w-[90vw] sm:w-[60vw] md:w-[50vw] lg:w-[40vw] xl:w-max p-8 xl:pl-8 xl:pr-2 h:auto xl:h-[70px] rounded-3xl xl:rounded-full backdrop-blur-[20px] flex flex-col xl:flex-row items-center gap-6 mx-auto text-sm  bg-[var(--color-secondary)]/80 text-[var(--color-text)] border boder-[var(--color-tertiary)]">
       {/**event search */}
       <EventSeacrh />
       {/**event lOCATION */}
@@ -15,11 +15,11 @@ const Searchbar = () => {
       {/**event date */}
       <div>Event DATE</div>
       {/**event TYPE */}
-      <div>Event TYPE</div>
+      <div className="text-[var(--color-text-muted)] hidden md:block">Event TYPE</div>
       {/**BTN SUBMIT  */}
       <button
         onClick={handleSubmit}
-        className="btn bg-[var(--color-accent)] hover:bg-[var(--color-accent-hover)] text-white "
+        className="btn bg-[var(--color-accent)] hover:bg-[var(--color-accent-hover)] font-medium text-white transition-colors "
       >
         Consultar
       </button>
