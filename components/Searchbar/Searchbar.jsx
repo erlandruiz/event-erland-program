cb389dbd9daf3a0b8844a9d8468146cb1aceb150
Erland Ruiz <erlandruiz@hotmail.com>
2025-12-03 22:13:48 -0500

    fourteen commit

--- components/Searchbar/Searchbar.jsx
+++ components/Searchbar/Searchbar.jsx
@@ -6,7 +6,7 @@ import EventLocation from "./EventLocation";
 const Searchbar = () => {
   const { handleSubmit } = useContext(EventContext);
   return (
-    <div className="bg-white w-[90vw] sm:w-[60vw] md:w-[50vw] lg:w-[40vw] xl:w-max p-8 xl:pl-8 xl:pr-2 h:auto xl:h-[70px] rounded-3xl xl:rounded-full backdrop-blur-[20px] flex flex-col xl:flex-row items-center gap-6 mx-auto text-sm">
+    <div className="bg-black w-[90vw] sm:w-[60vw] md:w-[50vw] lg:w-[40vw] xl:w-max p-8 xl:pl-8 xl:pr-2 h:auto xl:h-[70px] rounded-3xl xl:rounded-full backdrop-blur-[20px] flex flex-col xl:flex-row items-center gap-6 mx-auto text-sm">
       {/**event search */}
       <EventSeacrh />
       {/**event lOCATION */}
