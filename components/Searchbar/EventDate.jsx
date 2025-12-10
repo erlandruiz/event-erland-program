f44e5a7f5b1eba44f1936e0977954b828b16922a
Erland Ruiz <erlandruiz@hotmail.com>
2025-12-09 19:52:44 -0500

    thirthen one

--- components/Searchbar/EventDate.jsx
+++ components/Searchbar/EventDate.jsx
@@ -1,39 +1,61 @@
 "use client";
 
+import { useContext } from "react";
 import { format } from "date-fns";
+import { es } from "date-fns/locale";
+
+import { EventContext } from "@/contexts/EventContext";
+
 import { Button } from "../ui/button";
 import { Calendar } from "../ui/calendar";
-
-import { Popover } from "@/components/ui/popover";
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from "@/components/ui/popover";
 
 import { BiCalendar, BiChevronDown } from "react-icons/bi";
-import { useContext, useState } from "react";
-import { EventContext } from "@/contexts/EventContext";
-import { PopoverContent, PopoverTrigger } from "@radix-ui/react-popover";
+
 const EventDate = () => {
   const { selectedDate, setSelectedDate } = useContext(EventContext);
 
   const handleDateChange = (date) => {
-
-
     setSelectedDate(date);
-    
   };
+
   return (
-    <div className="flex w-full items-center gap-[10px] xl:w-[190px]">
+    <div className="flex items-center gap-[10px] w-full  xl:w-[190px] select-none">
       <div className="text-lg text-[var(--color-accent)]">
         <BiCalendar />
       </div>
+
       <Popover>
         <PopoverTrigger asChild>
-          <Button className="w-full justify-start p-0 bg-transparent hover:bg-transparent">
-            {selectedDate ? (
-              format(selectedDate, "PPP")
-            ) : (
-              <span>Seleccione una fecha</span>
-            )}
+          <Button
+            className=" 
+      flex items-center justify-between gap-2
+        h-9
+        bg-transparent border-none
+        px-0
+        text-sm text-[var(--color-text)]
+        shadow-xs
+        hover:bg-transparent
+
+  
+    "
+          >
+            <span className="truncate">
+              {selectedDate ? format(selectedDate, "d 'de' MMMM 'de' yyyy", {locale:es}) : "Fecha de eventos"}
+            </span>
+
+            <div >
+              <BiChevronDown />
+            </div>
+
+            {/* Ícono pegado al texto, como en SelectTrigger */}
           </Button>
         </PopoverTrigger>
+
         <PopoverContent className="w-auto p-0 bg-[var(--color-secondary)] border-0 text-[var(--color-text-muted)]">
           <Calendar
             mode="single"
@@ -42,11 +64,9 @@ const EventDate = () => {
             initialFocus
           />
         </PopoverContent>
-        <div className="text-[26px]">
-          <BiChevronDown />
-        </div>
       </Popover>
     </div>
   );
 };
+
 export default EventDate;
