5e3c85aa57a86aad995fd0cd4be64acb98e34dba
Erland Ruiz <erlandruiz@hotmail.com>
2025-12-10 17:06:00 -0500

    sixty seven

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,15 +1,15 @@
-import Image from "next/image";
+
 import Link from "next/link";
 import LogoErland from "./LogoErland";
 
 const Header = () => {
   return (
-    <div className="absolute left-0 right-0 z-10">
+    <div className="w-full left-0 right-0 z-10 bg-[var(--color-primary)]">
       <div className="container mx-auto h-full border-b border-[var(--color-secondary)] py-4 xl:py-6">
         <div className="flex justify-between items-center h-full">
 
             <Link href="/">
-                logo
+                <LogoErland/>
             </Link>
             <div className="flex gap-4">
                 <button className="btn bg-[var(--color-tertiary)]">Ingresar</button>
