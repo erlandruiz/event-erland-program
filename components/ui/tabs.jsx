7865def73ab058777d15d90c733e8784cfa85d56
Erland Ruiz <erlandruiz@hotmail.com>
2025-12-16 14:43:08 -0500

    eighteen three

--- components/ui/tabs.jsx
+++ components/ui/tabs.jsx
@@ -40,7 +40,7 @@ function TabsTrigger({
     <TabsPrimitive.Trigger
       data-slot="tabs-trigger"
       className={cn(
-        "bg-[var(--color-secondary)] h-[48px]   w-full data-[state=active]:bg-[var(--color-accent)] dark:data-[state=active]:text-foreground focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:outline-ring dark:data-[state=active]:border-input dark:data-[state=active]:bg-input/30 text-foreground dark:text-muted-foreground inline-flex  flex-1 items-center justify-center gap-2 rounded-full border border-transparent px-2 py-1 text-sm font-medium whitespace-nowrap transition-[color,box-shadow] focus-visible:ring-[3px] focus-visible:outline-1 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:shadow-sm [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+        "bg-[var(--color-secondary)] h-[48px]   w-full data-[state=active]:bg-[var(--color-accent)]  data-[state=active]:text-[var(--color-text)] dark:data-[state=active]:text-foreground focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:outline-ring dark:data-[state=active]:border-input dark:data-[state=active]:bg-input/30 text-foreground dark:text-muted-foreground inline-flex  flex-1 items-center justify-center gap-2 rounded-full border border-transparent px-2 py-1.5 text-sm font-normal whitespace-nowrap transition-[color,box-shadow] focus-visible:ring-[3px] focus-visible:outline-1 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:shadow-sm [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
         className
       )}
       {...props} />
